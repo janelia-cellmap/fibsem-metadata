5f7ca354e380159299a43d2e2906bbbf54900231
Davis Vann Bennett <davis.v.bennett@gmail.com>
2021-09-01 20:36:24 -0400

    update index now performs sync operations

--- src/fibsem_metadata/update_index.py
+++ src/fibsem_metadata/update_index.py
@@ -1,31 +1,47 @@
 # update index.json
 import json
 import click
+from pathlib import Path
 from fibsem_metadata.index import DatasetIndex
 from fibsem_metadata.index import DatasetIndex, VolumeSource, DatasetViewCollection
 from typing import Union, Any, Dict
 
-def materialize_element(json_blob: Dict[str, Any]) -> Union[VolumeSource, DatasetViewCollection]:
-    if 'views' in json_blob:
-        return DatasetViewCollection(**json_blob)
-    else:
-        return VolumeSource(**json_blob)
+def materialize_element(path: str) -> Union[VolumeSource, DatasetViewCollection]:
+    with open(path, mode='r') as fh:
+        json_blob = json.load(fh)
+        if 'views' in json_blob:
+            return DatasetViewCollection(**json_blob)
+        else:
+            return VolumeSource(**json_blob)
 
-
-def main(index_path: str, element_path: str):
-    old_index = DatasetIndex.parse_file(index_path)
-    new_index = old_index.copy()
-    with open(element_path, 'r') as fh:
-        new_element: Union[VolumeSource, DatasetViewCollection] = materialize_element(json.load(fh))
-
+def append_element(index: DatasetIndex, new_element: Union[VolumeSource, DatasetViewCollection]) -> DatasetIndex:
+    new_index = index.dict()
     if isinstance(new_element, VolumeSource):
-        new_volumes = list(new_index.volumes)
+        new_volumes = new_index['volumes']
         for idx, volume in enumerate(new_volumes):
             if volume.name == new_element.name:
                 new_volumes[idx] = new_element
-        new_index.volumes = new_volumes
+        new_index['volumes'] = new_volumes
     else:
-        new_index.views = new_element.views
+        new_index['views'] = new_element.views
+    return DatasetIndex(**new_index)
+
+def sync_elements(index: DatasetIndex, element_path: str) -> DatasetIndex:
+    new_index = index.dict()
+    new_index['volumes'] = sorted(list(map(materialize_element, Path(element_path).glob('*.json'))), key = lambda v: v.name)
+    return DatasetIndex(**new_index)
+
+def main(index_path: str, element_path: str):
+    old_index = DatasetIndex.parse_file(index_path)
+    
+    # syncing
+    if Path(element_path).is_dir():
+        new_index = sync_elements(old_index, element_path)
+    
+    # appending
+    else:
+        new_element: Union[VolumeSource, DatasetViewCollection] = materialize_element(element_path)
+        new_index = append_element(old_index, new_element)
 
     with open(index_path, mode='w') as fh:
         fh.write(new_index.json(indent=2))
@@ -33,7 +49,7 @@ def main(index_path: str, element_path: str):
 
 @click.command()
 @click.argument('index_path', type=click.Path(exists=True, dir_okay=False))
-@click.argument('element_path', type=click.Path(exists=True, dir_okay=False))
+@click.argument('element_path', type=click.Path(exists=True, dir_okay=True))
 def main_cli(index_path: str, element_path: str):
     return main(index_path, element_path)
 
