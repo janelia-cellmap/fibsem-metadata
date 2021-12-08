2583ed6b3f362f63abfa2ae10072b948b2156338
Davis Vann Bennett <davis.v.bennett@gmail.com>
2021-12-08 18:29:04 -0500

    remove manifests; tweak endpoint generation to target a separate dir

--- src/fibsem_metadata/generate_endpoints.py
+++ src/fibsem_metadata/generate_endpoints.py
@@ -7,7 +7,7 @@ from fibsem_metadata.models.manifest import DatasetManifest
 from fibsem_metadata.models.metadata import DatasetMetadata
 from fibsem_metadata.models.views import DatasetViews
 from fibsem_metadata.models.sources import VolumeSource
-
+from shutil import copyfile
 
 def validate_tree(root: str) -> None:
     """
@@ -25,13 +25,14 @@ def validate_tree(root: str) -> None:
             raise FileNotFoundError(f"Could not find {str(manifest_file)}")
 
 
-def build_manifest(dataset_path: Union[Path, str]) -> int:
+def build_manifest(dataset_path: Union[Path, str], output_dir: Union[Path, str]) -> int:
     root = Path(dataset_path)
-    manifest_path = root / "manifest.json"
+    name = root.parts[-1]
+    manifest_path = output_dir / "manifest.json"
     source_paths = (root / "sources").glob("*.json")
     metadata_path = root / "metadata.json"
     views_path = root / "views.json"
-    name = root.parts[-1]
+    thumbnail_path = root / "thumbnail.jpg"
 
     volumes = [materialize_element(path, VolumeSource) for path in source_paths]
     views = materialize_element(views_path, DatasetViews)
@@ -39,19 +40,24 @@ def build_manifest(dataset_path: Union[Path, str]) -> int:
     manifest = DatasetManifest(
         name=name, metadata=metadata, volumes=volumes, views=views.views
     )
+    
+    if not output_dir.exists():
+        output_dir.mkdir()
+    # write manifest and thumbnail to API destination
     manifest_path.write_text(manifest.json(indent=2))
+    copyfile(thumbnail_path, output_dir / thumbnail_path.name)
     return 0
 
 
 @click.command()
 @click.argument("root", type=click.Path(exists=True, file_okay=False))
-def main(root: str) -> int:
-    paths = tuple(filter(lambda v: v.is_dir(), Path(root).glob("*")))
+def main(root: str='.') -> int:
+    metadata_paths = tuple(filter(lambda v: v.is_dir(), Path(root).glob("metadata/*")))
     # generate the manifest
-    [build_manifest(path) for path in paths]
+    [build_manifest(path, Path("api") / path.name) for path in metadata_paths]
     # generate the index
-    index = Index(datasets=tuple(map(str, paths)))
-    with open(Path(root) / "index.json", mode="w") as fh:
+    index = Index(datasets=tuple(map(str, metadata_paths)))
+    with open(Path(root) / "api/index.json", mode="w") as fh:
         fh.write(index.json())
     return 0
 
