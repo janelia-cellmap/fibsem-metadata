a55e34fa70963dc7a1cf3e97f6b583b5f1bb1d4d
Davis Vann Bennett <davis.v.bennett@gmail.com>
2021-12-08 16:36:56 -0500

    black

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,10 +3,14 @@ from glob import glob
 import json
 import fsspec
 from typing import Any, Dict
-from fibsem_metadata.models.index import DatasetViewCollection, VolumeSource, DatasetView
+from fibsem_metadata.models.index import (
+    DatasetViewCollection,
+    VolumeSource,
+    DatasetView,
+)
 
-volume_sources = glob('metadata/datasets/*/sources/*')
-views = glob('metadata/datasets/*/views.json')
+volume_sources = glob("metadata/datasets/*/sources/*")
+views = glob("metadata/datasets/*/views.json")
 
 
 def exists_fsspec(path: str) -> bool:
@@ -14,12 +18,12 @@ def exists_fsspec(path: str) -> bool:
 
 
 def get_json_blob(path: str) -> Dict[str, Any]:
-    with open(path, mode='r') as fh:
+    with open(path, mode="r") as fh:
         blob = json.load(fh)
     return blob
 
 
-@pytest.mark.parametrize('path', volume_sources)
+@pytest.mark.parametrize("path", volume_sources)
 def test_volume_source(path: str):
     blob = get_json_blob(path)
     vsource = VolumeSource(**blob)
@@ -28,7 +32,8 @@ def test_volume_source(path: str):
     for subsource in vsource.subsources:
         assert exists_fsspec(subsource.path)
 
-@pytest.mark.parametrize('path', views)
+
+@pytest.mark.parametrize("path", views)
 def test_view(path: str):
     blob = get_json_blob(path)
     view = DatasetViewCollection(**blob)
