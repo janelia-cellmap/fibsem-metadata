92d548480fbe7d88a0807414db7b8569556ef9e7
Davis Vann Bennett <davis.v.bennett@gmail.com>
2022-02-23 19:03:17 -0500

    add better test for hyperlinks

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -2,17 +2,25 @@ import pytest
 from glob import glob
 import json
 import fsspec
+import requests
 from typing import Any, Dict
 from fibsem_metadata.models.metadata import DatasetMetadata
 from fibsem_metadata.models.views import DatasetViews
 from fibsem_metadata.models.sources import VolumeSource
+from itertools import chain
 
 volume_sources = glob('metadata/*/sources/*')
 views = glob('metadata/*/views.json')
 metadata = glob('metadata/*/metadata.json')
 
+
 def exists_fsspec(path: str) -> bool:
     return fsspec.get_mapper(path, anon=True).fs.exists(path)
+
+
+def not_404_status_code(url: str) -> bool:
+    rq = requests.get(url)
+    return not rq.status_code == 404
 
 
 def get_json_blob(path: str) -> Dict[str, Any]:
@@ -41,3 +49,5 @@ def test_view(views_path: str):
 def test_dataset_metadata(metadata_path: str):
     blob = get_json_blob(metadata_path)
     meta = DatasetMetadata(**blob)
+
+    assert all(not_404_status_code(link.href) for link in chain(meta.DOI, meta.publications))
