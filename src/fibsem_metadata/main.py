1823575f820ee901e015bb8db829b5244551cb14
Davis Vann Bennett <davis.v.bennett@gmail.com>
2022-07-15 09:22:09 -0400

    start cdk-ing

--- src/fibsem_metadata/main.py
+++ src/fibsem_metadata/main.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, FastAPI
 from fibsem_metadata.api.v1.api import api_router
 from fibsem_metadata.core.config import settings
 import click
+from mangum import Mangum
 import uvicorn
 
 app = FastAPI(title="Cellmap")
@@ -10,6 +11,7 @@ root_router = APIRouter()
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.include_router(root_router)
 
+handler = Mangum(app, lifespan='off')
 
 @click.command()
 @click.option('--host', default='0.0.0.0', help='Host for the uvicorn server')
