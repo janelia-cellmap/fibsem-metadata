4b2dd231c461259948f433d473c2496b4485e8ee
Davis Vann Bennett <davis.v.bennett@gmail.com>
2022-08-25 20:39:53 -0400

    add route for api health

--- src/fibsem_metadata/main.py
+++ src/fibsem_metadata/main.py
@@ -2,12 +2,28 @@ import click
 import uvicorn
 from fastapi import APIRouter, FastAPI
 from mangum import Mangum
-
 from fibsem_metadata.api.v1.api import api_router
 from fibsem_metadata.core.config import settings
+from typing import Dict
+
 
 app = FastAPI(title="Cellmap")
 root_router = APIRouter()
+
+
+@root_router.get('/health')
+def api_status() -> Dict[str, bool]:
+    from fibsem_metadata.db.session import engine
+    from sqlalchemy.exc import OperationalError
+    import fibsem_metadata
+    try:
+        engine.connect()
+        connected = True
+    except OperationalError:
+        connected = False
+
+    return {'database_connected': connected, 'version': fibsem_metadata.__version__}
+
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.include_router(root_router)
@@ -20,7 +36,7 @@ handler = Mangum(app, lifespan="off")
 @click.option("--log_level", default="debug", help="Logging level")
 @click.option("--reload", default=False, help="enable auto-reload")
 def main(host: str, port: int, log_level: str, reload: bool):
-    uvicorn.run(app, host=host, port=port, log_level=log_level, reload=reload)
+    uvicorn.run("main:app", host=host, port=port, log_level=log_level, reload=reload)
 
 
 if __name__ == "__main__":
